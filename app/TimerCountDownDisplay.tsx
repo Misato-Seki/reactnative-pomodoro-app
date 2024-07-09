17a82009da999b97558aeadb55fd2ce5352d2dd3
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2024-07-09 14:46:12 +0300

    update: index.tsx

---
+++ app/TimerCountDownDisplay.tsx
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+type Props = {
+  timerDate: Date;
+}
+
+export const TimerCountDownDisplay: React.FC<Props> = ({ timerDate }) => {
+  return (
+    <View>
+      <Text style={styles.timerCountDownText}>
+        {timerDate.getMinutes().toString().padStart(2, "0")}:
+        {timerDate.getSeconds().toString().padStart(2, "0")}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  timerCountDownText: {
+    fontSize: 40,
+    fontWeight: '800',
+    color: "#fff"
+  }
+})
